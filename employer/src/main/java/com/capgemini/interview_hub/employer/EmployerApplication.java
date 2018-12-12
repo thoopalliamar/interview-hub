26d1fa9f92e6ecd4e0a05e02ed2018ed93aeeb7a
Thoopalli <amarnath.thoopalli@capgemini.com>
2018-12-12 11:21:02 +0530

    initializers_added

---
+++ employer/src/main/java/com/capgemini/interview_hub/employer/EmployerApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.interview_hub.employer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployerApplication.class, args);
+	}
+}
---
+++ panelist/src/main/java/com/capgemini/interview_hub/panelist/PanelistApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.interview_hub.panelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanelistApplication.class, args);
+	}
+}
